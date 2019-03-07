8cbf2ab5f6a3f7aad8ad7b6377278af31095d329
Nicholas Foos <nfoos@rm-f.me>
2019-03-07 02:18:22 +0000

    refactor urls
    make resume app configure it's own urls and include them from the main
    project

--- djangoresume/urls.py
+++ djangoresume/urls.py
@@ -1,22 +1,7 @@
-"""djangoresume URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import url
 from django.contrib import admin
-from resume import views  # import my views
+from django.urls import include, path
+
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index, name='resume_home'),
+    path('admin/', admin.site.urls),
+    path('', include('resume.urls')),
 ]
